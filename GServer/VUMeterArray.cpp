e8268d647aa869233c02bf282bd1e5105c29fae9
Styck <Styck>
1998-07-19 12:25:15 +0000

    Cleaned up code removing NOTUSED code and defines

--- GServer/VUMeterArray.cpp
+++ GServer/VUMeterArray.cpp
@@ -9,6 +9,7 @@
 
 #include "stdafx.h"
 #include "GServer.h"
+#include "GServerDoc.h"
 #include "VUMeterArray.h"
 #include "Module.h"
 
@@ -22,7 +23,6 @@ static char THIS_FILE[]=__FILE__;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-#define NUM_VU_READ   4   // HARDCODE NUMBER OF VU'S TO READ : NOT USED ANYMORE
 
 CVUMeterArray::CVUMeterArray()
 {
@@ -37,55 +37,26 @@ CVUMeterArray::CVUMeterArray()
 	{
 		m_aVUReadData[i].wAddr=cModule.GetModuleAddress(i);							// the VUthread Reads from here(module address)
 		m_aVUReadData[i].cLock=0;							// if Zero nobody monitors this VU, so we don't need to read the damn thnig
-
-#ifdef NOTUSED
-		m_aVUReadData[i].iVUValue[0]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[1]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[2]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[3]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[4]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[5]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[6]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[7]=0;			// data. VUthread writes here
-#endif
 		m_aVUReadData[i].wPeakClipValue=0;		// data
     m_aVUReadData[i].wModuleIdx = i;
-	}
 
-  /////////////////////////////////////////////////
-  // Hardcode modules to read vu data from
-#ifdef JUNK
-	for(int j=0;j<NUM_VU_READ;j++)
-	{
-		m_aVUReadData[j].wAddr=33+j;							// the VUthread Reads from here(module address)
-//		m_aVUReadData[j].iVUType=1;							// the VUthread Reads from here(Pre, Post, Comp, Gate)
-		m_aVUReadData[j].wLock=1;							// if Zero nobody monitors this VU, so we don't need to read the damn thnig
-#ifdef NOTUSED
-		m_aVUReadData[i].iVUValue[0]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[1]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[2]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[3]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[4]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[5]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[6]=0;			// data. VUthread writes here
-		m_aVUReadData[i].iVUValue[7]=0;			// data. VUthread writes here
-#endif
-		m_aVUReadData[j].wPeakClipValue=0;		// data
-    m_aVUReadData[j].wModuleIdx = j;
 	}
-#endif
 
 }
+
 
 CVUMeterArray::~CVUMeterArray()
 {
 
 }
 
+
 int CVUMeterArray::GetFirstReadIdx()
 {
 	return 0;
 }
+
+
 
 int CVUMeterArray::GetNextReadIdx(int iNext)
 {
@@ -113,16 +84,6 @@ int CVUMeterArray::GetNextReadIdx(int iNext)
   {
     return 0xffffffff;
   }
-
-/*
-	if(m_iCurrentReadIndex >= NUM_VU_READ)
-	{
-		m_iCurrentReadIndex = 0;
-		return 0xffffffff;
-	}
-	else
-		return	m_iCurrentReadIndex;
-*/
   return	m_iCurrentReadIndex;
 }
 
