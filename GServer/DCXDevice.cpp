5c910f9f7bd1f98074b0ebffc9be793e9161d767
Styck <Styck>
1999-04-19 22:25:33 +0000

    SF Filmore again, new vu and contgrol values

--- GServer/DCXDevice.cpp
+++ GServer/DCXDevice.cpp
@@ -174,7 +174,7 @@ BOOL                bIoctlResult = FALSE;
 #endif
     if(bIsVU)
 //     CTekSleep(m_pDoc->m_dwBasedelay,m_pDoc->m_dwVudelay);
-     CTekSleep(9000,42);    // was 50
+     CTekSleep(9000,28);    // was 50
     else         
 //     CTekSleep(m_pDoc->m_dwBasedelay,m_pDoc->m_dwCtrldelay);
      CTekSleep(9000,68);    // was 65
